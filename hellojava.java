c409d3c3e263cba3fe8d77eb5548613b23a6099f
Administrator <dol.mouse92@gmail.com>
2018-03-19 06:36:20 +0700

    check

---
+++ hellojava.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Programming");
+        Scanner sc = new Scanner(System.in);
+    }
+}
